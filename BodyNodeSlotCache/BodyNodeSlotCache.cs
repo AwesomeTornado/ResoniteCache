f8a1ea995e61833bb2c579adc74f500ac8e2350b
LazyTeenStudios <harleypeterson@gmail.com>
2025-03-16 11:02:46 -0500

    update todo comment

--- BodyNodeSlotCache/BodyNodeSlotCache.cs
+++ BodyNodeSlotCache/BodyNodeSlotCache.cs
@@ -20,11 +20,10 @@ public class BNSC : ResoniteMod {
 		Harmony harmony = new Harmony("com.__Choco__.BodyNodeSlotCache");
 		harmony.PatchAll();
 		Msg("BNSC, BodyNodeSlotCache: Successfully initialized!");
-		//TODO: Add a listener to invalidate null hash table entries when a new slot is added
 		//Tested: changing avatars, equipping avatars, deleting avatars, equipping with dash, equipping in world.
 		//Changing avatars has problems, until the original avatar is deleted the slot ref will not change.
 		//however, it actually seems like it is not a problem? Idrk, but i think the slot is locked to the user and not to the avatar.
-		//Equipping from dash works great, original avatar is deleted, and regenerated due to not having a parent.
+		//Equipping from dash works great, original avatar is deleted, and regenerated when needed.
 	}
 
 	[HarmonyPatch(typeof(BodyNodeExtensions), "GetBodyNodeSlot")]
@@ -51,6 +50,7 @@ public class BNSC : ResoniteMod {
 			}
 			if (NullSlots.Contains(__state.hash)) {
 				__result = null;
+				__state.hash = 0;
 				return false;//skip original function
 			}
 			Msg("Slot not hashed, executing recursive search...");
@@ -58,18 +58,16 @@ public class BNSC : ResoniteMod {
 		}
 
 		static void Postfix(ref Slot __result, passThroughData __state) {
-			if (__state != 0) {
+			if (__state.hash != 0) {
 				if (__result is null) {
-					//TODO: Find some way to add a listener in order to invalidate this hash table entry when a new slot is added.
 					UserRoot root = __state.user.Root;
 					Slot rotSlot = root.Slot;
+					NullSlots.Add(__state.hash);
+					rotSlot.ChildAdded += OnChildAdded;
+					Msg("Result was null, added to null slot list");
+				} else {
 					bodyNodeSlots.Add(__state.hash, __result);
 					bodyNodeSlots_reversed.Add(__result, __state.hash);
-					__result.ChildAdded += OnChildAdded;
-
-				} else {
-					bodyNodeSlots.Add(__state, __result);
-					bodyNodeSlots_reversed.Add(__result, __state);
 					__result.Destroyed += OnObjectDestroyed;
 					Msg("Added new slot to hashtable");
 				}
@@ -78,8 +76,8 @@ public class BNSC : ResoniteMod {
 		}
 
 		private static void OnChildAdded(Slot slot, Slot child) {
-			bodyNodeSlots.Remove(bodyNodeSlots_reversed[slot]);
-			bodyNodeSlots_reversed.Remove(slot);
+			NullSlots.Clear();//TODO: this could possibly be improved by making null lists per user. 
+			Msg("Child added, clearing null slots");
 		}
 
 		private static void OnObjectDestroyed(IDestroyable destroyable) {
